7b9887a98a673c942d1b2916c2fced47381becf8
diana <diliang4783573@gmail.com>
2020-09-07 17:49:41 -0700

    add navigation links to home/about, removed body margin

--- sukinly/src/NavBar/index.js
+++ sukinly/src/NavBar/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      Some NavBar.
-    </div>
+    <nav className="NavBar">
+      <span className="NavBar-left-items"> 
+        <NavLink to="/">HOME</NavLink>
+      </span>
+      <span className="NavBar-right-items">
+        <NavLink to="/about">ABOUT</NavLink>
+      </span>
+    </nav>
   );
 };
 
