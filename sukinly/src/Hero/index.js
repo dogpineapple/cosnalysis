f51ff4aa64937c704d9adcfa15e65a6a78471ff0
diana <diliang4783573@gmail.com>
2020-09-15 21:29:05 -0700

    changed project name in hero

--- sukinly/src/Hero/index.js
+++ sukinly/src/Hero/index.js
@@ -7,7 +7,7 @@ function Hero() {
       <div className="Hero-image"></div>
       <div className="Hero-about">
         <h1 className="Hero-description Hero-description-p1 typewriter">Curious about your everyday products? </h1>
-        <h3 className="Hero-description Hero-description-p2">Sukin.ly provides statistical information on your skincare products with visuals.</h3>
+        <h3 className="Hero-description Hero-description-p2">Cosnalysis provides statistical information on your skincare products with visuals.</h3>
       </div>
     </div>
   );
