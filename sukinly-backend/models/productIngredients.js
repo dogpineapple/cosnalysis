79df1438b9696d4c95610b6379907b3e632c2588
diana <diliang4783573@gmail.com>
2020-10-01 16:04:46 -0700

    getIngredientsList method added into ProductIngredient model

--- sukinly-backend/models/productIngredients.js
+++ sukinly-backend/models/productIngredients.js
@@ -19,6 +19,19 @@ class ProductIngredient {
     });
   }
 
+  static async getIngredientsList(productName) {
+    const result = db.query(
+      `SELECT ingredient_name
+      FROM product_ingredients
+      WHERE product_name=$1`,
+      [productName]
+    );
+
+    if (result.rows.length === 0) return { error: `No ingredients list for ${productName} available.` };
+
+    return { ingredients_list: result.rows[0] };
+  }
+
 }
 
 module.exports = ProductIngredient;
